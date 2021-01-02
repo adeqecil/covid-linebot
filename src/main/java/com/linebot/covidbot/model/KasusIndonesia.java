9eaba9c86157a4b409e03ac6b84300dd63c2a830
adeqecil <43349767+adeqecil@users.noreply.github.com>
2021-01-03 04:23:40 +0700

    .

--- src/main/java/com/linebot/covidbot/model/KasusIndonesia.java
+++ src/main/java/com/linebot/covidbot/model/KasusIndonesia.java
@@ -1,5 +1,6 @@
 package com.linebot.covidbot.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -9,7 +10,7 @@ import java.text.NumberFormat;
 import java.util.List;
 import java.util.Map;
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KasusIndonesia {
 
     private String type, name;
@@ -53,9 +54,6 @@ public class KasusIndonesia {
         return formatter.format(fatal);
     }
 
-    @JsonProperty("regions")
-    public List<String> getRegions(){
-        return regions;
-    }
+
 }
 
--- src/main/java/com/linebot/covidbot/service/BotTemplate.java
+++ src/main/java/com/linebot/covidbot/service/BotTemplate.java
@@ -66,7 +66,7 @@ public class BotTemplate {
                     region = hospitals.get(i).getRegion();
 
                     column = new CarouselColumn(null, name, region, Arrays.asList(new MessageAction("Details",
-                            "[" + String.valueOf(i) + "]" + " Details : " + name.substring(0, Math.min(40,
+                            "[" + String.valueOf(i) + "]" + " Details : " + name.substring(0, Math.min(35,
                                     name.length()))))
                     );
                     carouselColumns.add(column);
